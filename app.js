a6f0ddabd492409a11954904b9c8b16bd0257324
smhn24 <nabavi.1383@gmail.com>
2022-02-28 23:14:17 +0330

    E60 (ejs layouts)

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const path = require('path');
 
 const express = require('express');
+const expressLayout = require('express-ejs-layouts');
 const dotEnv = require('dotenv');
 const morgan = require('morgan');
 
@@ -19,8 +20,10 @@ const app = express();
 if (process.env.NODE_ENV === 'development') app.use(morgan('dev'));
 
 //* View Engine
+app.use(expressLayout);
 app.set('view engine', 'ejs');
 app.set('views', 'views');
+app.set('layout', './layouts/mainLayout');
 
 //* Static folder
 app.use(express.static(path.join(__dirname, 'public')));
