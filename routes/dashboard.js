1f4621f9f7d978ae388e400af28fa57a32f5c07e
smhn24 <nabavi.1383@gmail.com>
2022-03-01 00:45:17 +0330

    E62 (Dashboard page)

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -2,6 +2,16 @@ const { Router } = require('express');
 
 const router = new Router();
 
+//* @desc Dashboard
+//* @route GET /dashboard/
+router.get('/', (req, res) => {
+	res.render('dashboard', {
+		pageTitle: 'بخش مدیریت | داشبورد',
+		path: '/dashboard',
+		layout: './layouts/dashboardLayout',
+	});
+});
+
 //* @desc Login page
 //* @route GET /dashboard/login
 router.get('/login', (req, res) => {
