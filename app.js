342f859556b8b44d586fa1ccf9850873a07dbd38
smhn24 <nabavi.1383@gmail.com>
2022-02-28 16:16:53 +0330

    E57 (database)

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const express = require('express');
 const dotEnv = require('dotenv');
 const morgan = require('morgan');
 
+const connectDB = require('./config/db');
 const indexRoutes = require('./routes');
 
 //* Load Config
 dotEnv.config({ path: './config/config.env' });
+
+//* Database conncection
+connectDB();
 
 const app = express();
 
