1fa5aa7f06067445aa11d1f4838fa05c462964d7
smhn24 <nabavi.1383@gmail.com>
2022-03-01 23:00:12 +0330

    E71 (delete fastest validator and complete validation)

--- routes/users.js
+++ routes/users.js
@@ -1,51 +1,8 @@
 const { Router } = require('express');
-const Validator = require('fastest-validator');
+
+const User = require('../models/user');
 
 const router = new Router();
-const v = new Validator();
-
-const schema = {
-	fullname: {
-		type: 'string',
-		trim: true,
-		min: 4,
-		max: 255,
-		messages: {
-			required: 'نام و نام کاربری الزامی است',
-			stringMax: 'نام و نام خانوادگی طولانی است',
-			stringMin: 'نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد',
-			stringMax: 'نام و نام خانوادگی طولانی است',
-		},
-	},
-	email: {
-		type: 'email',
-		normalize: true,
-		messages: {
-			required: 'آدرس ایمیل الزامی است',
-			emailEmpty: 'ایمیل نباید خالی باشد',
-		},
-	},
-	password: {
-		type: 'string',
-		min: 4,
-		max: 255,
-		messages: {
-			required: 'کلمه عبور الزامی است',
-			stringMin: 'کلمه عبور نباید کمتر از 4 کاراکتر باشد',
-			stringMax: 'کلمه عبور طولانی است',
-		},
-	},
-	confirmPasswrod: {
-		type: 'string',
-		min: 4,
-		max: 255,
-		messages: {
-			required: 'تکرار کلمه عبور الزامی است',
-			stringMin: 'تکرار کلمه عبور نباید کمتر از 4 کاراکتر باشد',
-			stringMax: 'تکرار کلمه عبور طولانی است',
-		},
-	},
-};
 
 //* @desc Login page
 //* @route GET /users/login
@@ -64,26 +21,20 @@ router.get('/register', (req, res) => {
 
 //* @desc Register handle
 //* @route POST /users/register
-router.post('/register', (req, res) => {
-	const validate = v.validate(req.body, schema);
-	const errArr = [];
-	if (validate == 'true') {
-		const { fullname, email, password, confirmPasswrod } = req.body;
-		if (password !== confirmPasswrod) {
-			errArr.push({ message: 'کلمه های عبور یکسان نیستند' });
-			return res.render('register', {
-				pageTitle: 'ثبت نام کاربر جدید',
-				path: '/register',
-				errors: errArr,
-			});
-		}
+router.post('/register', async (req, res) => {
+	try {
+		await User.userValidation(req.body);
 		res.redirect('/users/login');
-	} else {
-		console.log(validate);
-		res.render('register', {
+	} catch (err) {
+		console.log(err);
+		const errors = [];
+		err.inner.forEach((e) => {
+			errors.push({ name: e.path, message: e.message });
+		});
+		return res.render('register', {
 			pageTitle: 'ثبت نام کاربر جدید',
 			path: '/register',
-			errors: validate,
+			errors,
 		});
 	}
 });
