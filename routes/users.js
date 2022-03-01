3ff830920276d7586b8a901bfbb7107e1566c6a7
smhn24 <nabavi.1383@gmail.com>
2022-03-01 18:34:54 +0330

    E67 (complete yup validation and show errors to user)

--- routes/users.js
+++ routes/users.js
@@ -5,16 +5,20 @@ const router = new Router();
 
 const schema = Yup.object().shape({
 	fullname: Yup.string()
-		.required('نام و نام خانوادگی وارد نشده است')
-		.min(4)
-		.max(255),
-	email: Yup.string()
-		.email('ایمیل معتبر نیست')
-		.required('ایمیل وارد نشده است'),
-	password: Yup.string().required('کلمه عبور وارد نشده است').min(4).max(255),
+		.required('نام و نام خانوادگی الزامی است')
+		.min(4, 'نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد')
+		.max(255, 'نام و نام خانوادگی طولانی است'),
+	email: Yup.string().email('ایمیل معتبر نیست').required('ایمیل الزامی است'),
+	password: Yup.string()
+		.required('کلمه عبور وارد نشده است')
+		.min(4, 'کلمه عبور نباید کمتر از 4 کاراکتر باشد')
+		.max(255, 'کلمه عبور طولانی است'),
 	confirmPassword: Yup.string()
-		.required('تکرار کلمه عبور وارد نشده است')
-		.oneOf([Yup.ref('password'), null]),
+		.required('تکرار کلمه عبور الزامی است')
+		.oneOf(
+			[Yup.ref('password'), null],
+			'کلمه عبور و تکرار کلمه عبور یکسان نیستند',
+		),
 });
 
 //* @desc Login page
@@ -39,11 +43,15 @@ router.post('/register', (req, res) => {
 		.validate(req.body)
 		.then((result) => {
 			console.log(result);
-			res.send('All is good.');
+			res.redirect('/users/login');
 		})
 		.catch((err) => {
-			console.log(err);
-			res.send('error', { errors: err.errors });
+			console.log(err.errors);
+			res.render('register', {
+				pageTitle: 'ثبت نام کاربر جدید',
+				path: '/register',
+				errors: err.errors,
+			});
 		});
 });
 
