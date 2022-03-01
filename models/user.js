5fa8bcae787b66b75341a11ec8f8eb7f986a80de
smhn24 <nabavi.1383@gmail.com>
2022-03-02 00:16:25 +0330

    E72 (create user controller & validate file)

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-const Yup = require('yup');
+
+const { schema } = require('./secure/userValidation');
 
 const userSchema = new mongoose.Schema({
 	fullname: {
@@ -22,24 +23,6 @@ const userSchema = new mongoose.Schema({
 		type: Date,
 		default: Date.now,
 	},
-});
-
-const schema = Yup.object().shape({
-	fullname: Yup.string()
-		.required('نام و نام خانوادگی الزامی است')
-		.min(4, 'نام و نام خانوادگی نباید کمتر از 4 کاراکتر باشد')
-		.max(255, 'نام و نام خانوادگی طولانی است'),
-	email: Yup.string().email('ایمیل معتبر نیست').required('ایمیل الزامی است'),
-	password: Yup.string()
-		.required('کلمه عبور وارد نشده است')
-		.min(4, 'کلمه عبور نباید کمتر از 4 کاراکتر باشد')
-		.max(255, 'کلمه عبور طولانی است'),
-	confirmPassword: Yup.string()
-		.required('تکرار کلمه عبور الزامی است')
-		.oneOf(
-			[Yup.ref('password'), null],
-			'کلمه عبور و تکرار کلمه عبور یکسان نیستند',
-		),
 });
 
 userSchema.statics.userValidation = function (body) {
