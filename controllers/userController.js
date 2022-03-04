c16e7ca594016d6aeb501dc6ec955e5fcb8414b8
smhn24 <nabavi.1383@gmail.com>
2022-03-04 19:12:39 +0330

    E74 (hash password)

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,3 +1,5 @@
+const bcrypt = require('bcryptjs');
+
 const User = require('../models/user');
 
 exports.login = (req, res) => {
@@ -15,27 +17,37 @@ exports.createUser = async (req, res) => {
 	const errors = [];
 	try {
 		await User.userValidation(req.body);
-		const { email } = req.body;
+		const { fullname, password, email } = req.body;
 		const user = await User.findOne({ email });
 		if (user) {
-			errors.push({ message: 'کاربری با این ایمیل از قبل موجود است' });
+			errors.push({ message: 'کاربری با این ایمیل وجود دارد' });
 			return res.render('register', {
 				pageTitle: 'ثبت نام کاربر جدید',
 				path: '/register',
 				errors,
 			});
 		}
-		await User.create(req.body);
-		// const { fullname, email, password } = req.body;
-		// const user = new User({ fullname, email, password });
-		// user.save()
-		// 	.then((user) => {
-		// 		console.log(user);
-		// 		res.redirect('/users/login');
-		// 	})
-		// 	.catch((err) => {
+
+		const hash = await bcrypt.hash(password, 10);
+		await User.create({
+			fullname,
+			email,
+			password: hash,
+		});
+		res.redirect('/users/login');
+
+		// bcrypt.genSalt(10, (err, salt) => {
+		// 	if (err) throw err;
+		// 	bcrypt.hash(password, salt, async (err, hash) => {
 		// 		if (err) throw err;
+		// 		await User.create({
+		// 			fullname,
+		// 			email,
+		// 			password: hash,
+		// 		});
+		// 		res.redirect('/users/login');
 		// 	});
+		// });
 	} catch (err) {
 		console.log(err);
 		err.inner.forEach((e) => {
