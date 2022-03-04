7e265eaf877b24f3417e2024bdc45c1d137d162c
smhn24 <nabavi.1383@gmail.com>
2022-03-04 17:46:49 +0330

    E73 (add registered users to database)

--- controllers/userController.js
+++ controllers/userController.js
@@ -12,12 +12,32 @@ exports.register = (req, res) => {
 };
 
 exports.createUser = async (req, res) => {
+	const errors = [];
 	try {
 		await User.userValidation(req.body);
-		res.redirect('/users/login');
+		const { email } = req.body;
+		const user = await User.findOne({ email });
+		if (user) {
+			errors.push({ message: 'کاربری با این ایمیل از قبل موجود است' });
+			return res.render('register', {
+				pageTitle: 'ثبت نام کاربر جدید',
+				path: '/register',
+				errors,
+			});
+		}
+		await User.create(req.body);
+		// const { fullname, email, password } = req.body;
+		// const user = new User({ fullname, email, password });
+		// user.save()
+		// 	.then((user) => {
+		// 		console.log(user);
+		// 		res.redirect('/users/login');
+		// 	})
+		// 	.catch((err) => {
+		// 		if (err) throw err;
+		// 	});
 	} catch (err) {
 		console.log(err);
-		const errors = [];
 		err.inner.forEach((e) => {
 			errors.push({ name: e.path, message: e.message });
 		});
