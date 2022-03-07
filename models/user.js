1d895ee388affac84e535c2554d14c212a67dbe3
smhn24 <nabavi.1383@gmail.com>
2022-03-07 20:03:39 +0330

    E86 (create blog model)

--- models/user.js
+++ models/user.js
@@ -46,6 +46,4 @@ userSchema.pre('save', async function (next) {
 	}
 });
 
-const User = mongoose.model('User', userSchema);
-
-module.exports = User;
+module.exports = mongoose.model('User', userSchema);
