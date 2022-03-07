bac32a0b5430db735f0123d930fb49363bae90f8
smhn24 <nabavi.1383@gmail.com>
2022-03-07 20:19:48 +0330

    E87 (working on dashboard)

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,17 +1,12 @@
 const { Router } = require('express');
 const { authenticated } = require('../middlewares/auth');
 
+const adminController = require('../controllers/adminController');
+
 const router = new Router();
 
 //* @desc Dashboard
 //* @route GET /dashboard/
-router.get('/', authenticated, (req, res) => {
-	res.render('dashboard', {
-		pageTitle: 'بخش مدیریت | داشبورد',
-		path: '/dashboard',
-		layout: './layouts/dashboardLayout',
-		fullname: req.user.fullname,
-	});
-});
+router.get('/', authenticated, adminController.getDashboard);
 
 module.exports = router;
