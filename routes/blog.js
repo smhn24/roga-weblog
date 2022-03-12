27cd2239feba979e178365f9794414edb71ab1b0
smhn24 <nabavi.1383@gmail.com>
2022-03-13 00:04:30 +0330

    E109 (blog controller)

--- routes/blog.js
+++ routes/blog.js
@@ -1,11 +1,10 @@
 const { Router } = require('express');
+const blogController = require('../controllers/blogController');
 
 const router = new Router();
 
 //* @desc Weblog index page
 //* @route GET /
-router.get('/', (req, res) => {
-	res.render('index', { pageTitle: 'وبلاگ', path: '/' });
-});
+router.get('/', blogController.getIndex);
 
 module.exports = router;
