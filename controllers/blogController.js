5f1b5fe34035b06ad3638679e861aa05f1b32e73
smhn24 <nabavi.1383@gmail.com>
2022-03-16 21:30:55 +0330

    E113 (index paggination)

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -4,16 +4,31 @@ const { get500, get404 } = require('./errorController');
 const { truncate } = require('../utils/helpers');
 
 exports.getIndex = async (req, res) => {
+	const page = +req.query.page || 1;
+	const postPerPage = +req.query.limit || 2;
+
 	try {
-		const posts = await Blog.find({ status: 'public' }).sort({
-			createdAt: 'desc',
-		});
+		const numberOfPosts = await Blog.find({
+			status: 'public',
+		}).countDocuments();
+		const posts = await Blog.find({ status: 'public' })
+			.sort({
+				createdAt: 'desc',
+			})
+			.skip((page - 1) * postPerPage)
+			.limit(postPerPage);
 		res.render('index', {
 			pageTitle: 'وبلاگ',
 			path: '/',
 			posts,
 			formatDate,
 			truncate,
+			currentPage: page,
+			nextPage: page + 1,
+			previousPage: page - 1,
+			hasNextPage: postPerPage * page < numberOfPosts,
+			hasPreviousPage: page > 1,
+			lastPage: Math.ceil(numberOfPosts / postPerPage),
 		});
 	} catch (err) {
 		console.log(err);
