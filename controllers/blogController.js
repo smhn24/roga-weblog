3779443b04512dffd55c19a2fcde524d4be1727b
smhn24 <nabavi.1383@gmail.com>
2022-04-17 21:58:41 +0430

    Show category & post writer-config prettier

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -1,210 +1,233 @@
-const Yup = require('yup')
-const fetch = require('node-fetch')
+const Yup = require('yup');
+const fetch = require('node-fetch');
 
-const Blog = require('../models/Blog')
-const Comment = require('../models/Comment')
-const User = require('../models/User')
+const Blog = require('../models/Blog');
+const Comment = require('../models/Comment');
+const User = require('../models/User');
 
-const { formatDate } = require('../utils/jalali')
-const { get500, get404 } = require('./errorController')
-const { sendEmail } = require('../utils/mailer')
+const { formatDate } = require('../utils/jalali');
+const { get500, get404 } = require('./errorController');
+const { sendEmail } = require('../utils/mailer');
 
 exports.index = async (req, res) => {
-  const page = +req.query.page || 1
-  const postPerPage = +req.query.limit || 5
+	const page = +req.query.page || 1;
+	const postPerPage = +req.query.limit || 5;
 
-  try {
-    const numberOfPosts = await Blog.find({
-      status: 'public'
-    }).countDocuments()
-    const posts = await Blog.find({ status: 'public' })
-      .sort({
-        createdAt: 'desc'
-      })
-      .skip((page - 1) * postPerPage)
-      .limit(postPerPage)
-    res.render('index', {
-      pageTitle: 'وبلاگ',
-      path: '/',
-      posts,
-      formatDate,
-      currentPage: page,
-      nextPage: page + 1,
-      previousPage: page - 1,
-      hasNextPage: postPerPage * page < numberOfPosts,
-      hasPreviousPage: page > 1,
-      lastPage: Math.ceil(numberOfPosts / postPerPage)
-    })
-  } catch (err) {
-    console.log(err)
-    get500(req, res)
-  }
-}
+	try {
+		const numberOfPosts = await Blog.find({
+			status: 'public',
+		}).countDocuments();
+		const posts = await Blog.find({ status: 'public' })
+			.sort({
+				createdAt: 'desc',
+			})
+			.skip((page - 1) * postPerPage)
+			.limit(postPerPage)
+			.populate(['user', 'category']);
+		res.render('index', {
+			pageTitle: 'وبلاگ',
+			path: '/',
+			posts,
+			formatDate,
+			currentPage: page,
+			nextPage: page + 1,
+			previousPage: page - 1,
+			hasNextPage: postPerPage * page < numberOfPosts,
+			hasPreviousPage: page > 1,
+			lastPage: Math.ceil(numberOfPosts / postPerPage),
+		});
+	} catch (err) {
+		console.log(err);
+		get500(req, res);
+	}
+};
 
 exports.singlePost = async (req, res) => {
-  try {
-    const post = await Blog.findById(req.params.id).populate('user')
-    if (!post) return get404(req, res)
+	try {
+		const post = await Blog.findById(req.params.id).populate([
+			'user',
+			'category',
+		]);
+		if (!post) return get404(req, res);
 
-    const comments = await await Comment.find({ blog: req.params.id }).sort({ commentedAt: 'desc' }).populate('commenter')
+		const comments = await await Comment.find({ blog: req.params.id })
+			.sort({ commentedAt: 'desc' })
+			.populate('commenter');
 
-    res.render('blog/post', {
-      pageTitle: post.title,
-      path: '/post',
-      post,
-      formatDate,
-      comments
-    })
-  } catch (err) {
-    console.log(err)
-    get500(req, res)
-  }
-}
+		res.render('blog/post', {
+			pageTitle: post.title,
+			path: '/post',
+			post,
+			formatDate,
+			comments,
+		});
+	} catch (err) {
+		console.log(err);
+		get500(req, res);
+	}
+};
 
 exports.contactUs = (req, res) => {
-  res.render('common/contactUs', {
-    pageTitle: 'تماس با ما',
-    path: '/contact-us',
-    success: req.flash('success'),
-    errors: []
-  })
-}
+	res.render('common/contactUs', {
+		pageTitle: 'تماس با ما',
+		path: '/contact-us',
+		success: req.flash('success'),
+		errors: [],
+	});
+};
 
 exports.handleContactUs = async (req, res) => {
-  const errors = []
+	const errors = [];
 
-  const secretKey = process.env.CAPTCHA_SECRET
-  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body['g-recaptcha-response']}&remoteip=${req.connection.remoteAddress}`
+	const secretKey = process.env.CAPTCHA_SECRET;
+	const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body['g-recaptcha-response']}&remoteip=${req.connection.remoteAddress}`;
 
-  const { fullname, email, message } = req.body
+	const { fullname, email, message } = req.body;
 
-  const schema = Yup.object().shape({
-    fullname: Yup.string().required('وارد کردن نام و نام خانوادگی الزامی است'),
-    email: Yup.string().email('ایمیل معتبر نیست').required('وارد کردن ایمیل الزامی است'),
-    message: Yup.string().required('وارد کردن پیام الزامی است')
-  })
+	const schema = Yup.object().shape({
+		fullname: Yup.string().required(
+			'وارد کردن نام و نام خانوادگی الزامی است',
+		),
+		email: Yup.string()
+			.email('ایمیل معتبر نیست')
+			.required('وارد کردن ایمیل الزامی است'),
+		message: Yup.string().required('وارد کردن پیام الزامی است'),
+	});
 
-  try {
-    const response = await fetch(verifyUrl, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        contentType: 'application/x-www-form-urlencoded; charset=utf-8'
-      }
-    })
-    const json = await response.json()
-    console.log(json)
-    if (!json.success) {
-      errors.push({ field: 'global', message: 'مشکلی در کپچا وجود دارد' })
-      res.render('common/contactUs', {
-        pageTitle: 'تماس با ما',
-        path: '/contact-us',
-        success: req.flash('success'),
-        errors
-      })
-    }
-  } catch (err) {
-    errors.push({ field: 'global', message: 'مشکلی به جود آمده است' })
-    res.render('common/contactUs', {
-      pageTitle: 'تماس با ما',
-      path: '/contact-us',
-      success: req.flash('success'),
-      errors
-    })
-  }
+	try {
+		const response = await fetch(verifyUrl, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+			},
+		});
+		const json = await response.json();
+		console.log(json);
+		if (!json.success) {
+			errors.push({
+				field: 'global',
+				message: 'مشکلی در کپچا وجود دارد',
+			});
+			res.render('common/contactUs', {
+				pageTitle: 'تماس با ما',
+				path: '/contact-us',
+				success: req.flash('success'),
+				errors,
+			});
+		}
+	} catch (err) {
+		errors.push({ field: 'global', message: 'مشکلی به جود آمده است' });
+		res.render('common/contactUs', {
+			pageTitle: 'تماس با ما',
+			path: '/contact-us',
+			success: req.flash('success'),
+			errors,
+		});
+	}
 
-  try {
-    await schema.validate(req.body, { abortEarly: false })
+	try {
+		await schema.validate(req.body, { abortEarly: false });
 
-    sendEmail(email, fullname, 'پیام از طرف وبلاگ', `پیام کاربر: ${message} <br><br> ایمیل کاربر: ${email}`)
+		sendEmail(
+			email,
+			fullname,
+			'پیام از طرف وبلاگ',
+			`پیام کاربر: ${message} <br><br> ایمیل کاربر: ${email}`,
+		);
 
-    req.flash('success', 'پیام شما با موفقیت ارسال شد')
-    return res.redirect('/contact-us')
-  } catch (err) {
-    err.inner.forEach(e => {
-      errors.push({ field: e.path, message: e.message })
-    })
-    return res.render('common/contactUs', {
-      pageTitle: 'تماس با ما',
-      path: '/contact-us',
-      success: req.flash('success'),
-      errors
-    })
-  }
-}
+		req.flash('success', 'پیام شما با موفقیت ارسال شد');
+		return res.redirect('/contact-us');
+	} catch (err) {
+		err.inner.forEach((e) => {
+			errors.push({ field: e.path, message: e.message });
+		});
+		return res.render('common/contactUs', {
+			pageTitle: 'تماس با ما',
+			path: '/contact-us',
+			success: req.flash('success'),
+			errors,
+		});
+	}
+};
 
 exports.handleSearch = async (req, res) => {
-  const page = +req.query.page || 1
-  const postPerPage = +req.query.limit || 2
+	const page = +req.query.page || 1;
+	const postPerPage = +req.query.limit || 2;
 
-  try {
-    const numberOfPosts = await Blog.find({
-      status: 'public',
-      $text: { $search: req.body.search }
-    }).countDocuments()
-    const posts = await Blog.find({
-      status: 'public',
-      $text: { $search: req.body.search }
-    })
-      .sort({
-        createdAt: 'desc'
-      })
-      .skip((page - 1) * postPerPage)
-      .limit(postPerPage)
-    res.render('index', {
-      pageTitle: 'نتایج جستجو',
-      path: '/',
-      posts,
-      formatDate,
-      currentPage: page,
-      nextPage: page + 1,
-      previousPage: page - 1,
-      hasNextPage: postPerPage * page < numberOfPosts,
-      hasPreviousPage: page > 1,
-      lastPage: Math.ceil(numberOfPosts / postPerPage)
-    })
-  } catch (err) {
-    console.log(err)
-    get500(req, res)
-  }
-}
+	try {
+		const numberOfPosts = await Blog.find({
+			status: 'public',
+			$text: { $search: req.body.search },
+		}).countDocuments();
+		const posts = await Blog.find({
+			status: 'public',
+			$text: { $search: req.body.search },
+		})
+			.sort({
+				createdAt: 'desc',
+			})
+			.skip((page - 1) * postPerPage)
+			.limit(postPerPage);
+		res.render('index', {
+			pageTitle: 'نتایج جستجو',
+			path: '/',
+			posts,
+			formatDate,
+			currentPage: page,
+			nextPage: page + 1,
+			previousPage: page - 1,
+			hasNextPage: postPerPage * page < numberOfPosts,
+			hasPreviousPage: page > 1,
+			lastPage: Math.ceil(numberOfPosts / postPerPage),
+		});
+	} catch (err) {
+		console.log(err);
+		get500(req, res);
+	}
+};
 
 exports.handleComment = async (req, res) => {
-  try {
-    await Comment.commentValidation(req.body)
+	try {
+		await Comment.commentValidation(req.body);
 
-    const { text } = req.body
-    await Comment.create({
-      text,
-      commenter: req.user.id,
-      blog: req.params.blogId
-    })
+		const { text } = req.body;
+		await Comment.create({
+			text,
+			commenter: req.user.id,
+			blog: req.params.blogId,
+		});
 
-    res.status(200).json({ success: true, message: 'نظر شما با موفقیت ثبت شد' })
-  } catch (err) {
-    err.inner.forEach(e => {
-      errors.push({ field: e.path, message: e.message })
-    })
-    try {
-      const post = await Blog.findById(req.params.blogId).populate('user')
-      if (!post) return get404(req, res)
+		res.status(200).json({
+			success: true,
+			message: 'نظر شما با موفقیت ثبت شد',
+		});
+	} catch (err) {
+		err.inner.forEach((e) => {
+			errors.push({ field: e.path, message: e.message });
+		});
+		try {
+			const post = await Blog.findById(req.params.blogId).populate(
+				'user',
+			);
+			if (!post) return get404(req, res);
 
-      const comments = await Comment.find({
-        blog: req.params.id
-      })
-        .populate('commenter')
-        .sort({ commentedAt: 'desc' })
+			const comments = await Comment.find({
+				blog: req.params.id,
+			})
+				.populate('commenter')
+				.sort({ commentedAt: 'desc' });
 
-      res.render('blog/post', {
-        pageTitle: post.title,
-        path: '/post',
-        post,
-        formatDate,
-        comments,
-        errors
-      })
-    } catch (err) {
-      get500(req, res)
-    }
-  }
-}
+			res.render('blog/post', {
+				pageTitle: post.title,
+				path: '/post',
+				post,
+				formatDate,
+				comments,
+				errors,
+			});
+		} catch (err) {
+			get500(req, res);
+		}
+	}
+};
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,289 +1,317 @@
-const passport = require('passport')
-const fetch = require('node-fetch')
-const jwt = require('jsonwebtoken')
+const passport = require('passport');
+const fetch = require('node-fetch');
+const jwt = require('jsonwebtoken');
 
-const User = require('../models/User')
-const { sendEmail } = require('../utils/mailer')
-const { get404 } = require('./errorController')
-const { loginValidation } = require('../models/secure/loginValidation')
+const User = require('../models/User');
+const { sendEmail } = require('../utils/mailer');
+const { get404 } = require('./errorController');
+const { loginValidation } = require('../models/secure/loginValidation');
 
 exports.login = (req, res) => {
-  res.setHeader('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0')
-  res.render('auth/login', {
-    pageTitle: 'ورود به بخش مدیریت',
-    path: '/login',
-    success: req.flash('success')
-  })
-}
+	res.setHeader(
+		'Cache-Control',
+		'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0',
+	);
+	res.render('auth/login', {
+		pageTitle: 'ورود به بخش مدیریت',
+		path: '/login',
+		success: req.flash('success'),
+	});
+};
 
 exports.handleLogin = async (req, res, next) => {
-  // const errors = [];
-  // const secretKey = process.env.CAPTCHA_SECRET;
-  // const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body["g-recaptcha-response"]}&remoteip=${req.connection.remoteAddress}`;
+	// const errors = [];
+	// const secretKey = process.env.CAPTCHA_SECRET;
+	// const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body["g-recaptcha-response"]}&remoteip=${req.connection.remoteAddress}`;
 
-  // try {
-  //   await loginValidation.validate(req.body, { abortEarly: false });
+	// try {
+	//   await loginValidation.validate(req.body, { abortEarly: false });
 
-  //   const response = await fetch(verifyUrl, {
-  //     method: "POST",
-  //     headers: {
-  //       Accept: "application/json",
-  //       contentType: "application/x-www-form-urlencoded; charset=utf-8",
-  //     },
-  //   });
-  //   const json = await response.json();
-  //   if (json.success) {
-  passport.authenticate(
-    'local',
-    {
-      failureRedirect: '/users/login'
-    }
-    // (err, user, info) => {
-    //   if (err) {
-    //     errors.push({
-    //       field: "global",
-    //       message: info.message,
-    //     });
-    //     res.render("auth/login", {
-    //       pageTitle: "ورود به بخش مدیریت",
-    //       path: "/login",
-    //       errors,
-    //     });
-    //   }
-    //   if (!user) {
-    //     errors.push({
-    //       field: "global",
-    //       message: info.message,
-    //     });
-    //     res.render("auth/login", {
-    //       pageTitle: "ورود به بخش مدیریت",
-    //       path: "/login",
-    //       errors,
-    //     });
-    //   }
-    // }
-  )(req, res, next)
-  //   } else {
-  //     errors.push({
-  //       field: "global",
-  //       message: "مشکلی در کپچا وجود دارد",
-  //     });
-  //     res.render("auth/login", {
-  //       pageTitle: "ورود به بخش مدیریت",
-  //       path: "/login",
-  //       success: req.flash("success"),
-  //       errors,
-  //     });
-  //   }
-  // } catch (err) {
-  //   if (err.inner) {
-  //     err.inner.forEach((e) => {
-  //       errors.push({ field: e.path, message: e.message });
-  //     });
-  //   } else {
-  //     errors.push({ field: "global", message: "مشکلی روی داده است" });
-  //   }
-  //   res.render("auth/login", {
-  //     pageTitle: "ورود به بخش مدیریت",
-  //     path: "/login",
-  //     errors,
-  //   });
-  // }
-}
+	//   const response = await fetch(verifyUrl, {
+	//     method: "POST",
+	//     headers: {
+	//       Accept: "application/json",
+	//       contentType: "application/x-www-form-urlencoded; charset=utf-8",
+	//     },
+	//   });
+	//   const json = await response.json();
+	//   if (json.success) {
+	passport.authenticate(
+		'local',
+		{
+			failureRedirect: '/users/login',
+		},
+		// (err, user, info) => {
+		//   if (err) {
+		//     errors.push({
+		//       field: "global",
+		//       message: info.message,
+		//     });
+		//     res.render("auth/login", {
+		//       pageTitle: "ورود به بخش مدیریت",
+		//       path: "/login",
+		//       errors,
+		//     });
+		//   }
+		//   if (!user) {
+		//     errors.push({
+		//       field: "global",
+		//       message: info.message,
+		//     });
+		//     res.render("auth/login", {
+		//       pageTitle: "ورود به بخش مدیریت",
+		//       path: "/login",
+		//       errors,
+		//     });
+		//   }
+		// }
+	)(req, res, next);
+	//   } else {
+	//     errors.push({
+	//       field: "global",
+	//       message: "مشکلی در کپچا وجود دارد",
+	//     });
+	//     res.render("auth/login", {
+	//       pageTitle: "ورود به بخش مدیریت",
+	//       path: "/login",
+	//       success: req.flash("success"),
+	//       errors,
+	//     });
+	//   }
+	// } catch (err) {
+	//   if (err.inner) {
+	//     err.inner.forEach((e) => {
+	//       errors.push({ field: e.path, message: e.message });
+	//     });
+	//   } else {
+	//     errors.push({ field: "global", message: "مشکلی روی داده است" });
+	//   }
+	//   res.render("auth/login", {
+	//     pageTitle: "ورود به بخش مدیریت",
+	//     path: "/login",
+	//     errors,
+	//   });
+	// }
+};
 
 exports.rememberMe = (req, res) => {
-  if (req.body.remember) {
-    req.session.cookie.originalMaxAge = 24 * 60 * 60 * 1000
-  } else {
-    req.session.cookie.expire = null
-  }
+	if (req.body.remember) {
+		req.session.cookie.originalMaxAge = 24 * 60 * 60 * 1000;
+	} else {
+		req.session.cookie.expire = null;
+	}
 
-  res.redirect('/dashboard')
-}
+	res.redirect('/dashboard');
+};
 
 exports.logout = (req, res) => {
-  req.session.destroy(() => {
-    req.logout()
-    res.setHeader('Cache-Control', 'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0')
-    res.redirect('/users/login')
-  })
-}
+	req.session.destroy(() => {
+		req.logout();
+		res.setHeader(
+			'Cache-Control',
+			'no-cache, private, no-store, must-revalidate, max-stale=0, post-check=0, pre-check=0',
+		);
+		res.redirect('/users/login');
+	});
+};
 
 exports.register = (req, res) => {
-  res.render('auth/register', {
-    pageTitle: 'ثبت نام کاربر جدید',
-    path: '/register'
-  })
-}
+	res.render('auth/register', {
+		pageTitle: 'ثبت نام کاربر جدید',
+		path: '/register',
+	});
+};
 
 exports.createUser = async (req, res) => {
-  const errors = []
+	const errors = [];
 
-  const secretKey = process.env.CAPTCHA_SECRET
-  const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body['g-recaptcha-response']}&remoteip=${req.connection.remoteAddress}`
+	const secretKey = process.env.CAPTCHA_SECRET;
+	const verifyUrl = `https://google.com/recaptcha/api/siteverify?secret=${secretKey}&response=${req.body['g-recaptcha-response']}&remoteip=${req.connection.remoteAddress}`;
 
-  try {
-    const response = await fetch(verifyUrl, {
-      method: 'POST',
-      headers: {
-        Accept: 'application/json',
-        contentType: 'application/x-www-form-urlencoded; charset=utf-8'
-      }
-    })
-    const json = await response.json()
-    if (!json.success) {
-      errors.push({ field: 'global', message: 'مشکلی در کپچا وجود دارد' })
-      return res.render('auth/register', {
-        pageTitle: 'ثبت نام کاربر جدید',
-        path: '/register',
-        errors
-      })
-    }
-  } catch (err) {
-    errors.push({ field: 'global', message: 'مشکلی به جود آمده است' })
-    return res.render('auth/register', {
-      pageTitle: 'ثبت نام کاربر جدید',
-      path: '/register',
-      errors
-    })
-  }
+	try {
+		const response = await fetch(verifyUrl, {
+			method: 'POST',
+			headers: {
+				Accept: 'application/json',
+				contentType: 'application/x-www-form-urlencoded; charset=utf-8',
+			},
+		});
+		const json = await response.json();
+		if (!json.success) {
+			errors.push({
+				field: 'global',
+				message: 'مشکلی در کپچا وجود دارد',
+			});
+			return res.render('auth/register', {
+				pageTitle: 'ثبت نام کاربر جدید',
+				path: '/register',
+				errors,
+			});
+		}
+	} catch (err) {
+		errors.push({ field: 'global', message: 'مشکلی به جود آمده است' });
+		return res.render('auth/register', {
+			pageTitle: 'ثبت نام کاربر جدید',
+			path: '/register',
+			errors,
+		});
+	}
 
-  try {
-    await User.userValidation(req.body)
-    const { fullname, password, email } = req.body
-    const user = await User.findOne({ email })
-    if (user) {
-      errors.push({
-        field: 'global',
-        message: 'کاربری با این ایمیل وجود دارد'
-      })
-      return res.render('auth/register', {
-        pageTitle: 'ثبت نام کاربر جدید',
-        path: '/register',
-        errors
-      })
-    }
+	try {
+		await User.userValidation(req.body);
+		const { fullname, password, email } = req.body;
+		const user = await User.findOne({ email });
+		if (user) {
+			errors.push({
+				field: 'global',
+				message: 'کاربری با این ایمیل وجود دارد',
+			});
+			return res.render('auth/register', {
+				pageTitle: 'ثبت نام کاربر جدید',
+				path: '/register',
+				errors,
+			});
+		}
 
-    await User.create({ fullname, email, password })
+		await User.create({ fullname, email, password });
 
-    //? Send welcome email
-    sendEmail(email, fullname, 'به سایت وبلاگ خوش آمدید', 'خیلی خوشحالیم که به جمع ما پیوستید. امیدوارم وبلاگ های زیبایی توسط شما منتشر بشه.')
+		//? Send welcome email
+		sendEmail(
+			email,
+			fullname,
+			'به سایت وبلاگ خوش آمدید',
+			'خیلی خوشحالیم که به جمع ما پیوستید. امیدوارم وبلاگ های زیبایی توسط شما منتشر بشه.',
+		);
 
-    req.flash('success', 'ثبت نام با موفقیت انجام شد')
-    res.redirect('/users/login')
-  } catch (err) {
-    err.inner.forEach(e => {
-      errors.push({ field: e.path, message: e.message })
-    })
-    return res.render('auth/register', {
-      pageTitle: 'ثبت نام کاربر جدید',
-      path: '/register',
-      errors
-    })
-  }
-}
+		req.flash('success', 'ثبت نام با موفقیت انجام شد');
+		res.redirect('/users/login');
+	} catch (err) {
+		err.inner.forEach((e) => {
+			errors.push({ field: e.path, message: e.message });
+		});
+		return res.render('auth/register', {
+			pageTitle: 'ثبت نام کاربر جدید',
+			path: '/register',
+			errors,
+		});
+	}
+};
 
 exports.forgetPassword = async (req, res) => {
-  res.render('auth/forgetPassword', {
-    pageTitle: 'فراموشی رمز عبور',
-    path: '/forget-password',
-    success: req.flash('success')
-  })
-}
+	res.render('auth/forgetPassword', {
+		pageTitle: 'فراموشی رمز عبور',
+		path: '/forget-password',
+		success: req.flash('success'),
+	});
+};
 
 exports.handleForgetPassword = async (req, res) => {
-  let errors = []
-  const { email } = req.body
+	let errors = [];
+	const { email } = req.body;
 
-  if (!email.length > 0) {
-    errors.push({ field: 'email', message: 'ایمیل را وارد کنید' })
-    res.render('auth/forgetPassword', {
-      pageTitle: 'فراموشی رمز عبور',
-      path: '/forget-password',
-      errors
-    })
-  }
+	if (!email.length > 0) {
+		errors.push({ field: 'email', message: 'ایمیل را وارد کنید' });
+		res.render('auth/forgetPassword', {
+			pageTitle: 'فراموشی رمز عبور',
+			path: '/forget-password',
+			errors,
+		});
+	}
 
-  try {
-    const user = await User.findOne({ email })
-    if (!user) {
-      errors.push({ field: 'email', message: 'کاربری با این ایمیل وجود ندارد' })
-      res.render('auth/forgetPassword', {
-        pageTitle: 'فراموشی رمز عبور',
-        path: '/forget-password',
-        errors
-      })
-    }
+	try {
+		const user = await User.findOne({ email });
+		if (!user) {
+			errors.push({
+				field: 'email',
+				message: 'کاربری با این ایمیل وجود ندارد',
+			});
+			res.render('auth/forgetPassword', {
+				pageTitle: 'فراموشی رمز عبور',
+				path: '/forget-password',
+				errors,
+			});
+		}
 
-    const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
-      expiresIn: '1h'
-    })
+		const token = jwt.sign({ userId: user.id }, process.env.JWT_SECRET, {
+			expiresIn: '1h',
+		});
 
-    const resetLink = `http://weblog.nabavi.dev/users/reset-password/${token}`
-    sendEmail(user.email, user.fullname, 'فراموشی رمز عبور', `<h1>برای تغییر رمز عبور روی لینک زیر کلیک کنید</h1> <a href="${resetLink}">لینک تغییر رمز عبور</a>`)
+		const resetLink = `http://weblog.nabavi.dev/users/reset-password/${token}`;
+		sendEmail(
+			user.email,
+			user.fullname,
+			'فراموشی رمز عبور',
+			`<h1>برای تغییر رمز عبور روی لینک زیر کلیک کنید</h1> <a href="${resetLink}">لینک تغییر رمز عبور</a>`,
+		);
 
-    req.flash('success', 'لینک تغییر رمز عبور به ایمیل شما ارسال شد')
-    res.redirect('/users/forget-password')
-  } catch (err) {
-    errors.push({ field: 'email', message: 'مشکلی به جود آمده است' })
-    res.render('auth/forgetPassword', {
-      pageTitle: 'فراموشی رمز عبور',
-      path: '/forget-password',
-      errors
-    })
-  }
-}
+		req.flash('success', 'لینک تغییر رمز عبور به ایمیل شما ارسال شد');
+		res.redirect('/users/forget-password');
+	} catch (err) {
+		errors.push({ field: 'email', message: 'مشکلی به جود آمده است' });
+		res.render('auth/forgetPassword', {
+			pageTitle: 'فراموشی رمز عبور',
+			path: '/forget-password',
+			errors,
+		});
+	}
+};
 
 exports.resetPassword = async (req, res) => {
-  const token = req.params.token
+	const token = req.params.token;
 
-  let decodedToken
-  try {
-    decodedToken = jwt.verify(token, process.env.JWT_SECRET)
-  } catch (err) {
-    if (!decodedToken) {
-      return get404(req, res)
-    }
-  }
+	let decodedToken;
+	try {
+		decodedToken = jwt.verify(token, process.env.JWT_SECRET);
+	} catch (err) {
+		if (!decodedToken) {
+			return get404(req, res);
+		}
+	}
 
-  res.render('auth/resetPassword', {
-    pageTitle: 'تغییر رمز عبور',
-    path: '/reset-password',
-    userId: decodedToken.userId
-  })
-}
+	res.render('auth/resetPassword', {
+		pageTitle: 'تغییر رمز عبور',
+		path: '/reset-password',
+		userId: decodedToken.userId,
+	});
+};
 
 exports.handleResetPassword = async (req, res) => {
-  let errors = []
-  const { password, confirmPassword } = req.body
+	let errors = [];
+	const { password, confirmPassword } = req.body;
 
-  if (password !== confirmPassword) {
-    errors.push({ field: 'confirmPassword', message: 'رمز عبور با تکرار آن مطابقت ندارد' })
-    return res.render('auth/resetPassword', {
-      pageTitle: 'تغییر رمز عبور',
-      path: '/reset-password',
-      userId: req.params.id,
-      errors
-    })
-  }
-  try {
-    const user = await User.findById(req.params.id)
-    if (!user) {
-      return get404(req, res)
-    }
+	if (password !== confirmPassword) {
+		errors.push({
+			field: 'confirmPassword',
+			message: 'رمز عبور با تکرار آن مطابقت ندارد',
+		});
+		return res.render('auth/resetPassword', {
+			pageTitle: 'تغییر رمز عبور',
+			path: '/reset-password',
+			userId: req.params.id,
+			errors,
+		});
+	}
+	try {
+		const user = await User.findById(req.params.id);
+		if (!user) {
+			return get404(req, res);
+		}
 
-    user.password = password
-    await user.save()
+		user.password = password;
+		await user.save();
 
-    req.flash('success', 'رمز عبور با موفقیت به روز رسانی شد')
-    res.redirect('/users/login')
-  } catch (err) {
-    errors.push({ field: 'confirmPassword', message: 'مشکلی به جود آمده است' })
-    return res.render('auth/resetPassword', {
-      pageTitle: 'تغییر رمز عبور',
-      path: '/reset-password',
-      userId: req.params.id,
-      errors
-    })
-  }
-}
+		req.flash('success', 'رمز عبور با موفقیت به روز رسانی شد');
+		res.redirect('/users/login');
+	} catch (err) {
+		errors.push({
+			field: 'confirmPassword',
+			message: 'مشکلی به جود آمده است',
+		});
+		return res.render('auth/resetPassword', {
+			pageTitle: 'تغییر رمز عبور',
+			path: '/reset-password',
+			userId: req.params.id,
+			errors,
+		});
+	}
+};
